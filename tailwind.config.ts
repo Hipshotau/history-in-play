3fffe2ceb2829e2176b617593d23b863c25c2cf0
Hipshotau <qzw13@txstate.edu>
2025-08-21 10:21:12 -0500

    Fix global background color issues

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,18 @@
 // tailwind.config.js
 export default {
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false,
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: '#FFD700',
+        dark: '#0f0f0f',
+      },
+    },
   },
   plugins: [],
 }
